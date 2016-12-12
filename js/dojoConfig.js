42e57e70203f8409207d7d8a88a73c774e25debf
ManueleAbstract <manuele.cozzolino@asbtract.it>
2016-12-12 15:07:35 +0100

    first commit

---
+++ js/dojoConfig.js
@@ -0,0 +1,10 @@
+var dojoConfig = {
+    async: 1,
+    // cacheBust: 1,
+    parseOnLoad: true,
+    tlmSiblingOfDojo: false,
+    packages: [
+        { name: "dojo", location: "../libs/dojo/dojo" },
+        { name: "bootstrap", location: "../libs/dojo-bootstrap" }
+    ]
+};
